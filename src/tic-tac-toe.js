37e520aa75895ff2dec3cfe3864bdfac82690825
Liliya <liliyavoloshine@gmail.com>
2021-07-06 20:40:53 +0400

    feat: add winner-detector logic

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -9,24 +9,61 @@ class TicTacToe {
   }
 
   getCurrentPlayerSymbol() {
-    console.log('get current player symb')
     return this.currentPlayer
   }
 
   nextTurn(rowIndex, columnIndex) {
+    // draw symbol in clicked ceil
     this.matrix[rowIndex][columnIndex] = this.currentPlayer
-    // console.log(this.marks)
 
+    // change symbol to opposite
     if (this.currentPlayer === 'x') {
-      this.currentPlayer ='o'
+      this.currentPlayer = 'o'
     } else {
       this.currentPlayer = 'x'
     }
+
+    console.log(rowIndex, columnIndex)
   }
 
   isFinished() {}
 
-  getWinner() {}
+  getWinner() {
+    // check first row
+    if (this.matrix[0][0] === this.matrix[0][1] && this.matrix[0][1]  === this.matrix[0][2]) {
+      return this.matrix[0][0]
+    } else
+    // check second row
+    if (this.matrix[1][0] === this.matrix[1][1] && this.matrix[1][1]  === this.matrix[1][2]) {
+      return this.matrix[1][0]
+    } else
+    // check third row
+    if (this.matrix[2][0] === this.matrix[2][1] && this.matrix[2][1]  === this.matrix[2][2]) {
+      return this.matrix[2][0]
+    } else
+    // check first column
+    if (this.matrix[0][0] === this.matrix[1][0] && this.matrix[1][0]  === this.matrix[2][0]) {
+      return this.matrix[0][0]
+    } else
+    // check second column
+    if (this.matrix[0][1] === this.matrix[1][1] && this.matrix[1][1]  === this.matrix[2][1]) {
+      return this.matrix[0][1]
+    } else
+    // check third column
+    if (this.matrix[0][2] === this.matrix[1][2] && this.matrix[1][2]  === this.matrix[2][2]) {
+      return this.matrix[0][2]
+    } else
+    // check left-right diagonal
+    if (this.matrix[0][0] === this.matrix[1][1] && this.matrix[1][1]  === this.matrix[2][2]) {
+      return this.matrix[0][0]
+    } else
+    // check right-left diagonal
+    if (this.matrix[0][2] === this.matrix[1][1] && this.matrix[1][1]  === this.matrix[2][0]) {
+      return this.matrix[0][2]
+    } else {
+      return null
+    }
+  }
 
   noMoreTurns() {}
 
